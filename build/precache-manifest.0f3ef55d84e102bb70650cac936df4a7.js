df59e808200957cdfb487562aa2ee69d0a5cfb10
eldrssn <maxkalimullin@gmail.com>
2021-08-24 22:20:51 +0300

    add build

---
+++ build/precache-manifest.0f3ef55d84e102bb70650cac936df4a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5acb1da8fc21b78ffe6c19e9cc28f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c681871672cceb4acba",
+    "url": "/static/css/main.82243409.chunk.css"
+  },
+  {
+    "revision": "a452e8567986c56a0133",
+    "url": "/static/js/2.9f891233.chunk.js"
+  },
+  {
+    "revision": "6c681871672cceb4acba",
+    "url": "/static/js/main.3bdf081c.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "45ab2799ba811ebd8effc6cefaead0e0",
+    "url": "/static/media/down.45ab2799.svg"
+  },
+  {
+    "revision": "2a5cfc3e6cfbfa4a29b113252299520c",
+    "url": "/static/media/up.2a5cfc3e.svg"
+  }
+]);
